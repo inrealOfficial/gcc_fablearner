3ec276a4b7e36148a82c72acf3acffcdeed2015a
Aryan Agarwal <aryan23062001@gmail.com>
2025-06-09 17:24:44 +0530

    fb-pixels-update-2

---
+++ src/app/payment/success/metadata.ts
@@ -0,0 +1,6 @@
+import { metadata as layoutMetadata } from "@/app/layout";
+
+export const metadata = {
+  ...layoutMetadata,
+  title: "Payment Successful !! - FabLearner",
+};
--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -1,15 +1,9 @@
 "use client";
 
 import { CheckoutSuccess } from "@/components/sections/checkout-succes";
-import { metadata as layoutMetadata } from "@/app/layout";
 import { ConversionTracker } from "@/components/ConversionTracker";
 import { useEffect } from "react";
 import { trackFBEvent } from "@/components/FacebookPixel";
-
-export const metadata = {
-  ...layoutMetadata,
-  title: "Payment Successful !! - FabLearner",
-};
 
 export default function Page() {
   useEffect(() => {
